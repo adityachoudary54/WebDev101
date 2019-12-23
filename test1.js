13377566b928f057513d76a1fead9af22ad05ef5
Aditya choudhary <adityachoudhary54@gmail.com>
2019-12-23 23:20:07 +0530

    added ch9 form content

---
+++ test1.js
@@ -0,0 +1,24 @@
+console.log("Variable Declaration");
+var name = "Adarsh";
+var nam;
+nam = "Aj";
+var na;
+
+console.log(name, nam, na);
+
+const name1 = "AJJha";
+//name1='A'; This gives ab=n error
+
+//let keyword
+let name2 = "A";
+console.log("let keyword:", name2);
+
+//let vs var
+
+console.log("let vs var:");
+var num = 2;
+{
+  let num = 3;
+  console.log(num);
+}
+console.log(num);
